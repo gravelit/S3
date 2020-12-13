e1ee13704b1421aea569b3b2584d40cb046b7f88
gravelit <graveline17@gmail.com>
2020-12-13 11:51:24 -0700

    Add multipart upload, but don't utilize threading

--- s3_upload.py
+++ s3_upload.py
@@ -2,11 +2,14 @@
 # Imports
 # ----------------------------------------------------------------------------------
 import boto3
+from boto3.s3.transfer import TransferConfig
 from botocore.exceptions import NoCredentialsError
 import coloredlogs
 import configparser
 import logging
 import os
+import sys
+import threading
 
 # ----------------------------------------------------------------------------------
 # Types
@@ -41,6 +44,11 @@ class S3Api:
                                aws_access_key_id=self._access_key,
                                aws_secret_access_key=self._secret_key)
 
+        # Initialize resource
+        self.s3_resource = boto3.resource('s3',
+                                          aws_access_key_id=self._access_key,
+                                          aws_secret_access_key=self._secret_key)
+
     # ------------------------------------------------------------------------------
     def upload_files(self):
         for bucket in self.uploads:
@@ -53,13 +61,50 @@ class S3Api:
         if not s3_file:
             s3_file = os.path.basename(local_file)
 
-        try:
-            self.s3.upload_file(local_file, bucket, s3_file)
-            logger.info("Uploaded {} to S3".format(local_file))
-        except FileNotFoundError:
-            logger.error("The file {} was not found".format(local_file))
-        except NoCredentialsError:
-            logger.error("Credentials not available")
+        if os.path.getsize(local_file) < 1073741824:  # File size is less than a gig
+            try:
+                self.s3.upload_file(local_file, bucket, s3_file)
+                logger.info("\nUploaded {} to S3".format(local_file))
+            except FileNotFoundError:
+                logger.error("\nThe file {} was not found".format(local_file))
+            except NoCredentialsError:
+                logger.error("\nCredentials not available")
+        else:
+            try:
+                self._multipart_upload(local_file, bucket, s3_file)
+                logger.info("\nUploaded {} to S3".format(local_file))
+            except FileNotFoundError:
+                logger.error("\nThe file {} was not found".format(local_file))
+            except NoCredentialsError:
+                logger.error("\nCredentials not available")
+
+    # ------------------------------------------------------------------------------
+    def _multipart_upload(self, local_file, bucket, s3_file):
+        transfer_config = TransferConfig(multipart_threshold=1024 * 25,
+                                         multipart_chunksize=1024 * 25,
+                                         use_threads=False)
+        self.s3_resource.Object(bucket, s3_file).upload_file(local_file,
+                                                             Config=transfer_config,
+                                                             Callback=ProgressPercentage(local_file))
+
+class ProgressPercentage(object):
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+
+    def __call__(self, bytes_amount):
+        # To simplify we'll assume this is hooked up
+        # to a single filename.
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (
+                    self._filename, self._seen_so_far, self._size,
+                    percentage))
+            sys.stdout.flush()
 
 # ----------------------------------------------------------------------------------
 # Globals
